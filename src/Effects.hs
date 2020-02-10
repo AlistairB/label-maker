99609533306737c46aac756a1ae1c7638907b608
Alistair Burrowes <alistair.burrowes@gmail.com>
2020-02-11 10:14:20 +1100

    Defined the effects

--- src/Effects.hs
+++ src/Effects.hs
@@ -1,29 +1,68 @@
 {-# OPTIONS_GHC -fno-warn-missing-export-lists #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Effects where
+
+import Control.Effect.Class (HFunctor, Effect)
+import GHC.Generics (Generic1)
 
 import Types.LabelConfig
 import Types.RepoUpdatePlan
 import Types.FetchedData
 
-data ReadRawLabelConfig m a where
-  PerformRead :: ReadRawLabelConfig m RawLabelConfig
+newtype ReadRawLabelConfig m k
+  = PerformRead (String -> m k)
+  deriving (Functor, Generic1)
 
-data DecodeInputData m a where
-  PerformDecode :: RawLabelConfig -> DecodeInputData m LabelMakerConfig
+instance HFunctor ReadRawLabelConfig
+instance Effect   ReadRawLabelConfig
 
-data FetchOrgRepos m a where
-  PerformFetchOrgRepos :: LabelMakerConfig -> FetchOrgRepos m FetchedAllData
+-- data ReadRawLabelConfig m a where
+--   PerformRead :: ReadRawLabelConfig m RawLabelConfig
 
-data ProduceUpdatePlans m a where
-  ProduceUpdatePlans :: LabelMakerConfig -> FetchedAllData -> ProduceUpdatePlans m LabelMakerUpdatePlan
+newtype DecodeInputData m k
+  = PerformDecode (RawLabelConfig -> LabelMakerConfig -> m k)
+  deriving (Functor, Generic1)
 
-data UpdateLabels m a where
-  PerformLabelUpdate :: LabelMakerUpdatePlan -> UpdateLabels m ()
+instance HFunctor DecodeInputData
+instance Effect   DecodeInputData
 
-makeSem ''ReadRawLabelConfig
-makeSem ''DecodeInputData
-makeSem ''ProduceUpdatePlans
-makeSem ''FetchOrgRepos
-makeSem ''UpdateLabels
+-- data DecodeInputData m a where
+--   PerformDecode :: RawLabelConfig -> DecodeInputData m LabelMakerConfig
+
+newtype FetchOrgRepos m k
+  = PerformFetchOrgRepos (LabelMakerConfig -> FetchedAllData -> m k)
+  deriving (Functor, Generic1)
+
+instance HFunctor FetchOrgRepos
+instance Effect   FetchOrgRepos
+
+-- data FetchOrgRepos m a where
+--   PerformFetchOrgRepos :: LabelMakerConfig -> FetchOrgRepos m FetchedAllData
+
+newtype ProduceUpdatePlans m k
+  = ProduceUpdatePlans (LabelMakerConfig -> FetchedAllData -> LabelMakerUpdatePlan -> m k)
+  deriving (Functor, Generic1)
+
+instance HFunctor ProduceUpdatePlans
+instance Effect   ProduceUpdatePlans
+
+-- data ProduceUpdatePlans m a where
+--   ProduceUpdatePlans :: LabelMakerConfig -> FetchedAllData -> ProduceUpdatePlans m LabelMakerUpdatePlan
+
+data UpdateLabels m k
+  = PerformLabelUpdate LabelMakerUpdatePlan (m k)
+  deriving (Functor, Generic1)
+
+instance HFunctor UpdateLabels
+instance Effect   UpdateLabels
+
+-- data UpdateLabels m a where
+--   PerformLabelUpdate :: LabelMakerUpdatePlan -> UpdateLabels m ()
+
+-- makeSem ''ReadRawLabelConfig
+-- makeSem ''DecodeInputData
+-- makeSem ''ProduceUpdatePlans
+-- makeSem ''FetchOrgRepos
+-- makeSem ''UpdateLabels
